8d6c813a9602dcd449e2349235216a264365fa67
Charlot0316 <2280583075@qq.com>
2021-07-02 14:51:36 +0800

    初始化

---
+++ src/database/Entity/Dish.java
@@ -0,0 +1,4 @@
+package database.Entity;
+
+public class Dish {
+}
---
+++ src/utils/Load.java
@@ -0,0 +1,21 @@
+package utils;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class Load {
+    public static Image image(String path) {
+        BufferedImage img = null;
+        URL url = Load.class.getResource("/res/" + path);
+        try {
+            img = ImageIO.read(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return img;
+    }
+
+}
